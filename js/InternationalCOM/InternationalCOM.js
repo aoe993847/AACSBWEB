b84f88a7808daa1d91ee4816b65571f625d04b4b
yao <aoe553@gmail.com>
2021-03-21 14:38:21 +0800

    修改 nav

--- js/InternationalCOM/InternationalCOM.js
+++ js/InternationalCOM/InternationalCOM.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
     var a = $("#pagelink").offset().top; 
 
     $(document).scroll(function(){ 
-     if($(this).scrollTop() > 50) 
+     if($(this).scrollTop() > 80) 
      { 
+        $('#pagelink').addClass("navbar-fixed-top");; 
       $('#pagelink').css({"background":"rgba(0, 0, 0, 0.541)"}); 
-     } else { 
+     } else if($(this).scrollTop() < 50){ 
       $('#pagelink').css({"background":"black"}); 
+      $('#pagelink').removeClass("navbar-fixed-top");;
      } 
     }); 
    }); 
