7386c63c5bce553aca9f6bdd0a3fddac05c04867
wqsaxzedc5 <wqsaxzedc5@gmail.com>
2021-03-24 23:30:08 +0800

    aacsb

---
+++ js/AACSBAccreditation/AACSBAccreditation.js
@@ -0,0 +1,26 @@
+
+
+$('.filtering').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    autoplay:true,
+    autoplaySpeed: 200,  
+    
+
+  });
+  
+  var filtered = false;
+  
+  $('.js-filter').on('click', function(){
+    if (filtered === false) {
+      $('.filtering').slick('slickFilter',':even');
+      $(this).text('Unfilter Slides');
+      filtered = true;
+    } else {
+      $('.filtering').slick('slickUnfilter');
+      $(this).text('Filter Slides');
+      filtered = false;
+    }
+  });
